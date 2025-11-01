b2f91d823d4cfe333e56a4786781efe4179b593f
zitch8 <darendasallas27@gmail.com>
2025-11-01 18:09:19 +0800

    refactor: article model

--- models/article.py
+++ models/article.py
@@ -14,8 +14,8 @@ class Article():
     category: str
     priority: str
     
-    def __post__init__(self):
-        self.validate()
+    # def __post__init__(self):
+    #     self.validate()
 
     def validate(self):
         if not isinstance(self.id, str):
@@ -28,5 +28,12 @@ class Article():
             raise ValueError("Article category must be a string")
         if not isinstance(self.priority, str):
             raise ValueError("Article priority must be a string")
+        else:
+            try:
+                self.priority = Priority(self.priority.lower())
+            except ValueError:
+                raise ValueError("Article priority must be one of: high, medium, low")
+            
+        return True
 
         # TODO: Parse priority to Priority class
